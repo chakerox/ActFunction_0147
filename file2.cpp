79696af5077cb20766d18e3c70418e8a04f748be
chakerox <chakerox@gmail.com>
2026-03-03 20:43:27 +0700

    implementasi prosedur

--- file2.cpp
+++ file2.cpp
@@ -3,3 +3,18 @@ using namespace std;
 
 // deklarasi global
 int p, l;
+// Implementasi Prosedur dan Fungsi
+void input() {
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
+
+float jumlah(float a, float b, float c) {
+    return a + b + c;
+}
+
+int luasPersegiP(int a, int b) {
+    return a * b;
+}

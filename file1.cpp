eae871046732087b3008033e264746137f1a9641
chakerox <chakerox@gmail.com>
2026-03-03 20:39:11 +0700

    mengimplementasi prosedur dan fungsi

--- file1.cpp
+++ file1.cpp
@@ -4,3 +4,17 @@ using namespace std;
 
 // deklarasi global
 int p, l, luas;
+// Implementasi Prosedur dan Fungsi
+void input() {
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
+
+int luasPersegi() {
+    return p * l;
+}
+void output() {
+    cout << "Hasilnya = " << luasPersegi() << endl;
+    cout << "terima kasih";
